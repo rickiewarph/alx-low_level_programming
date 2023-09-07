05d771d34702c012bc031969c042862c17f8eaf1
rickiewarph <rickiewarph95@gmail.com>
2023-09-07 15:59:31 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - refers to the main function in this program
+ *
+ * Return: should be 0 all the time
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
