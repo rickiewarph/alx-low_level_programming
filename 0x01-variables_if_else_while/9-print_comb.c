84b9f1619ca436d9800b21d3223af7efa5c73a1d
rickiewarph <rickiewarph95@gmail.com>
2023-09-08 15:28:00 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,14 @@ int main(void)
 
 	for (r = 0 ; r < 10 ; r++)
 	{
-		putchar(r + '0');
-		putchar(',');
-		putchar(' ');
+		if (r == 9)
+			putchar(r + '0');
+		else
+		{
+		      putchar(r + '0');
+		      putchar(',');
+		      putchar(' ');
+	        }
 	}
 	return (0);
 }
