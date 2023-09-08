4b64d8a2551f82493d8ef4b52440d230c24e8616
rickiewarph <rickiewarph95@gmail.com>
2023-09-08 14:35:29 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the program
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int r;
+
+	for (r = 0; r < 10; r++)
+		printf("%d", r);
+	printf("\n");
+	return (0);
+}
