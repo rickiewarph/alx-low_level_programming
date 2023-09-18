fe1a11dbb686349974124552772cfb8b3748af0a
rickiewarph <rickiewarph95@gmail.com>
2023-09-18 03:31:39 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - finds string length
+ * @s: string the function counts
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	
+	for (i = 0 ; s(i) != '\0' ; i++)
+		count++;
+	return (count);
+}
