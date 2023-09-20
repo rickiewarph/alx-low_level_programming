fc9236448e4a8aa65156f1bf9cb32095aa615be0
rickiewarph <rickiewarph95@gmail.com>
2023-09-20 02:21:35 -0700

    11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,41 +11,43 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int overflow = 0, i, j, k = 0;
+	int val1, val2, temp_tot, len1 = 0, len2 = 0;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+	i = len1 - 1;
+	j = len2 - 1;
+	if (len1 >= size_r || len2 >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+	while (i >= 0 || j >= 0 || overflow)
 	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
+		val1 = (i >= 0) ? (n1[i] - '0') : 0;
+		val2 = (j >= 0) ? (n2[j] - '0') : 0;
+
 		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
+		overflow = temp_tot / 10;
+
+		if (k < size_r - 1)
+			r[k] = (temp_tot % 10) + '0';
+
+		k++;
 		i--;
+		j--;
 	}
-	if (digits == size_r)
+	if (k == size_r)
 		return (0);
-	*(r + digits) = '\0';
+	r[k] = '\0';
+	for (i = 0, j = k - 1; i < j; i++, j--)
+	{
+		char temp = r[i];
+
+		r[i] = r[j];
+		r[j] = temp;
+	}
+
 	return (r);
 }
+
