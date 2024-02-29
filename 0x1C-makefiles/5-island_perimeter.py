9cc7aa6bb0ab322ae7c82e7047961f9a7f31f46f
rickiewarph <rickiewarph95@gmail.com>
2024-02-29 00:34:36 -0800

    3, 4, 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defining an island's perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """Returning the island perimeter.
+
+    The grid to rep water by 0 and land by 1.
+
+    Args:
+        grid (list): A list of list of ints representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    wdth = len(grid[0])
+    heght = len(grid)
+    edgs = 0
+    sze = 0
+
+    for m in range(heght):
+        for n in range(wdth):
+            if grid[m][n] == 1:
+                size += 1
+                if (n > 0 and grid[m][n - 1] == 1):
+                    edgs += 1
+                if (m > 0 and grid[m - 1][n] == 1):
+                    edgs += 1
+    return size * 4 - edgs * 2
